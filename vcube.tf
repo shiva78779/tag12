e3f20682680401c21457130952860ea10ec654de
Venna12 <52368332+Venna12@users.noreply.github.com>
2023-08-08 18:56:22 +0530

    Update vcube.tf

--- vcube.tf
+++ vcube.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # Create a bucket
 resource "aws_s3_bucket" "b1" {
 
-  bucket = "s3-raining123"
+  bucket = "s3-chintha-bucket"
 
   acl    = "private"   # or can be "public-read"
 
