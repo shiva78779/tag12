defb0156001c6aeb6c084f0ef4b9da42f860aa69
shiva <119577331+shiva78779@users.noreply.github.com>
2023-08-18 17:04:10 +0530

    Update vcube.tf

--- vcube.tf
+++ vcube.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # Create a bucket
 resource "aws_s3_bucket" "b1" {
 
-  bucket = "s3-shiva1-bucket"
+  bucket = "s3-shiva123-bucket"
 
   acl    = "private"   # or can be "public-read"
 
