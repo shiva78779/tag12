d36e37ef4d42ff430bba0cc90ddad4fcc18b5449
shiva <119577331+shiva78779@users.noreply.github.com>
2023-08-18 16:34:23 +0530

    Update vcube.tf

--- vcube.tf
+++ vcube.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3JSZOJBQ3JDNG43U"
-  secret_key = "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"
+  access_key = "AKIA6HZQGGSHVF2RYJVH"
+  secret_key = "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"
 }
 
 # Create a bucket
 resource "aws_s3_bucket" "b1" {
 
-  bucket = "s3-chinthasdfghjcvbnm-bucket"
+  bucket = "s3-shiva1-bucket"
 
   acl    = "private"   # or can be "public-read"
 
@@ -20,3 +20,4 @@ resource "aws_s3_bucket" "b1" {
   }
 
 }
+
